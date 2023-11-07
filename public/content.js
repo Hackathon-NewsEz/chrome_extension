fc1ac3e5bddcf73cf5030d02a9060715d2c91d7f
ljs7143 <junseo27643779@gmail.com>
2023-11-07 19:44:46 +0900

    still doesn't work

--- public/content.js
+++ public/content.js
@@ -3,10 +3,13 @@ fetch('http://localhost:3000/difficulty_testing')
     .then(data => {
         console.log(data);
         var sentence = data.difficult;
-        var bodyText = document.body.innerHTML;
-        var regex = new RegExp(sentence, "g"); // 정규식으로 변경
-        var highlighted = '<span style="background-color: yellow;">' + sentence + '</span>';
-        bodyText = bodyText.replace(regex, highlighted); // 모든 일치하는 문장을 하이라이트로 변경
-        document.body.innerHTML = bodyText;
+        var paragraphs = document.body.getElementsByTagName('p'); // 문장이 포함되어 있을 것으로 예상되는 태그를 선택
+        for (var i = 0; i < paragraphs.length; i++) {
+            if (paragraphs[i].textContent.includes(sentence)) { // 해당 태그의 텍스트에 문장이 포함되어 있는지 확인
+                var originalText = paragraphs[i].textContent;
+                var highlightedText = originalText.replace(sentence, `<span style="background-color: yellow;">${sentence}</span>`); // 문장을 하이라이트하는 HTML로 변경
+                paragraphs[i].innerHTML = highlightedText; // 해당 태그의 HTML을 변경
+            }
+        }
     })
     .catch(error => console.error(error));
